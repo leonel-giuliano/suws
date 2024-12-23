0149519232547f65910f66a0f3ff9b3958aec739
leonel-giuliano <129548024+leonel-giuliano@users.noreply.github.com>
2024-12-23 15:06:33 -0300

    Imitate getline

--- include/file.h
+++ include/file.h
@@ -2,11 +2,21 @@
 #define __SUWS_FILE_H__
 
 
-#include <stddef.h>
+#include <stdio.h>
 
 
 #define APPLIST_PATH "suws.list"
 
+
+// Imitates 'getline'.
+// Allocates the buffer if it wasn't allocated and scans until a
+// new line is encountered.
+// If the size is smaller than the line size, it gets reallocated.
+// On success, returns 0.
+// If the end of the file was encountered, returns EOF.
+// On failure, returns 1 if there was a problem allocating, with the
+// parameters or reading the file
+int fscanline(char **_BufferPtr, size_t *_SizePtr, FILE *_Fp);
 
 // Returns a pointer to the applist path
 char *getapp_path(char *_Buffer, size_t _Size);
--- src/file.c
+++ src/file.c
@@ -1,7 +1,46 @@
-#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include "file.h"
+
+
+int fscanline(char **pbuf, size_t *pn, FILE *fp) {
+    char    c;
+    size_t  i = 0;
+
+    // Allocates the buffer if it was passed without
+    // being allocated before
+    if(*pbuf == NULL) {
+        if(!*pn) return 0;
+        if((*pbuf = (char *)malloc(*pn * sizeof(char))) == NULL)
+            return 1;
+    } else if(!*pn) return 1;
+
+    // The '-1' is because it needs space for the '\0'
+    while(i < *pn - 1 && (c = getc(fp)) != '\n' && c != EOF)
+        (*pbuf)[i++] = c;
+
+    // If the line was greater than the buffer size
+    if(c != '\n' && c != EOF) {
+        // Count the amount of chars left
+        size_t n = 0;
+        while((c = getc(fp)) != '\n' && c != EOF) n++;
+
+        (*pn) += n;
+        if(n && (*pbuf = (char *)realloc(*pbuf, *pn * sizeof(char))) == NULL)
+            return 1;
+
+        // Go back and scan the data
+        // The '-1' is beacuse the '\n' doesn't get saved
+        fseek(fp, -1 * (n + 1), SEEK_CUR);
+        if(fgets(*pbuf + i, n, fp) == NULL) return 1;
+    }
+
+    (*pbuf)[i] = '\0';
+
+    return (c == EOF) ? EOF : 0;
+}
 
 
 char *getapp_path(char *buf, size_t n) {
