a842d560ff1663ddfc344dbb22a3859ea9749bfc
leonel-giuliano <129548024+leonel-giuliano@users.noreply.github.com>
2024-12-23 13:57:18 -0300

    Open file

--- src/suws.c
+++ src/suws.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include <linux/limits.h>
+
+#include "file.h"
 
 
-void execapps(int wsnum) {
-    FILE *fp;
+int execapps(int wsnum) {
+    int     e = 0;
+    char    path[PATH_MAX];
+    FILE    *fp;
+
+    if((fp = fopen(getapp_path(path, sizeof(path)), "r")) == NULL)
+        return -1;
+
+    close_fp: if(fclose(fp)) e = -1;
+    return e;
 }
